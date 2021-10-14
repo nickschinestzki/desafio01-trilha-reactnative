599ce6a813e5654f8af600896acb5ac6f83df6d0
Schin Nick <nickdoncatto@gmail.com>
2021-10-14 11:28:06 -0300

    desafio concluído :)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,38 +6,48 @@ import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
 export function Home() {
-  const [tasks, setTasks] = useState<Task[]>([]);
+   const [tasks, setTasks] = useState<Task[]>([]);
 
-  function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
-  }
+   function handleAddTask(newTaskTitle: string) {
+      setTasks(old => [...old, {
+         id: new Date().getTime(),
+         title: newTaskTitle,
+         done: false,
+      }])
+   }
 
-  function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
-  }
+   function handleToggleTaskDone(id: number) {
+      const updatedTasks = tasks.map(task =>
+         task.id === id ?
+            { ...task, done: !task.done } :
+            { ...task }
+      )
 
-  function handleRemoveTask(id: number) {
-    //TODO - remove task from state
-  }
+      setTasks(updatedTasks)
+   }
 
-  return (
-    <View style={styles.container}>
-      <Header tasksCounter={tasks.length} />
+   function handleRemoveTask(id: number) {
+      setTasks(old => old.filter(t => t.id !== id))
+   }
 
-      <TodoInput addTask={handleAddTask} />
+   return (
+      <View style={styles.container}>
+         <Header tasksCounter={tasks.length} />
 
-      <TasksList 
-        tasks={tasks} 
-        toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
-      />
-    </View>
-  )
+         <TodoInput addTask={handleAddTask} />
+
+         <TasksList
+            tasks={tasks}
+            toggleTaskDone={handleToggleTaskDone}
+            removeTask={handleRemoveTask}
+         />
+      </View>
+   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
+   container: {
+      flex: 1,
+      backgroundColor: '#EBEBEB'
+   }
 })
